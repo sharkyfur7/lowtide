7c98a9dc240d6e5c969bebb829a0438c87c32bde
Sharky <sharkyblacktip@protonmail.com>
2025-03-25 18:05:20 +0100

    Updated header title

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -1,5 +1,5 @@
 <header class="mb-2 flex flex-col items-center">
-	<h1 class="mb-2 text-5xl text-blue-500">lowtide</h1>
+	<h1 class="mb-2 text-5xl text-blue-500">lowtide.dev</h1>
 
 	<nav class="flex justify-center gap-4 font-mono">
 		<a href="/">home</a>
