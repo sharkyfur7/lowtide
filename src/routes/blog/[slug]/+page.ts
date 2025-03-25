7adfa87cf25d9570177abbdbad46dde4f962b857
Sharky <sharkyblacktip@protonmail.com>
2025-03-25 13:49:39 +0100

    Fixed import path for posts

--- src/routes/blog/[slug]/+page.ts
+++ src/routes/blog/[slug]/+page.ts
@@ -1,5 +1,5 @@
 export async function load({ params }) {
-	const post = await import(`/src/post/${params.slug}.md`);
+	const post = await import(`$lib/../post/${params.slug}.md`);
 	const { title, description, date } = post.metadata;
 	const Content = post.default;
 
