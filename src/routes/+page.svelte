58b47971493f5827c3790e17f8a712175b7c7fc1
Sharky <sharkyblacktip@protonmail.com>
2025-03-25 18:05:38 +0100

    Added the no ai webring

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -89,6 +89,48 @@
 			</a>
 		</div>
 	</section>
+
+	<section class="flex justify-center">
+		<map name="noaitxtwidget">
+			<area
+				href="https://baccyflap.com/noai"
+				target="_blank"
+				shape="rect"
+				coords="28,0,219,30"
+				alt="no ai webring"
+				title="no ai webring"
+			/>
+			<area
+				href="https://baccyflap.com/noai/?prv&s=shr"
+				target="_top"
+				shape="rect"
+				coords="0,0,27,30"
+				alt="previous"
+				title="previous"
+			/>
+			<area
+				href="https://baccyflap.com/noai/?rnd"
+				target="_top"
+				shape="rect"
+				coords="221,6,235,25"
+				alt="random"
+				title="random"
+			/>
+			<area
+				href="https://baccyflap.com/noai/?nxt&s=shr"
+				target="_top"
+				shape="rect"
+				coords="239,0,263,30"
+				alt="next"
+				title="next"
+			/>
+		</map>
+		<img
+			usemap="#noaitxtwidget"
+			src="https://baccyflap.com/noai/txtwidget-light.gif"
+			alt="a white rectangular box with a dashed outline with the words 'the no ai webring' in the centre, with a parenthetical question mark next to it and two arrows pointing left and right to either side"
+		/>
+	</section>
 </main>
 
 <Footer></Footer>
